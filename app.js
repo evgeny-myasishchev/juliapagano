33092d57e369e71a01184f256f632a2c95e00634
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-01-19 01:28:22 +0200

    Defining app structure.

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
-var express = require('express');
-var app = express();
-var morgan = require('morgan');
+var boot = require('./lib/boot');
 
-app.use(morgan('combined'));
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-});
-
-var server = app.listen(3000, function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log('Example app listening at http://%s:%s', host, port)
-
-});
+boot.newApp()
+	.withAccessLog()
+	.withControllers()
+	.start({port: 3000});
