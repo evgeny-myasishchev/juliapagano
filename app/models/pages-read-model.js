1ddb62d0e304f8e5a703feaae2df729deacc3dca
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-02-03 00:13:30 +0200

    Adding pages read model stub.

---
+++ app/models/pages-read-model.js
@@ -0,0 +1,13 @@
+var PagesReadModel = function(db) {
+	this.getPages = function() {
+		
+	};
+	
+	this.getBlocks = function(pageId) {
+		//TODO: Not implemented
+	};
+};
+
+module.exports = {
+	PagesReadModel: PagesReadModel
+};
