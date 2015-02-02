08726352682c50f148727b238df9857df4920de5
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2015-02-03 01:48:02 +0200

    Adding dummy logic to route pages.

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var boot = require('./lib/boot');
 var express = require('express');
+var controllers = require('./app/controllers');
 
 boot.newApp()
 	.withLogging()
-	.withControllers(require('./app/controllers'))
+	// .withControllers(controllers)
 	.withViewEngine()
 	.withAssets()
 	.withDb()
 	.withReadModels()
+	.withPages(controllers.pages_controller)
 	.withServer(3000)
 	.start(express());
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,15 @@
-exports.home = {
-	index: function(req, res) {
-		res.render('home/index');
+// exports.home = {
+// 	index: function(req, res) {
+// 		res.render('home/index');
+// 	}
+// }
+
+exports.pages_controller = {
+	createPageAction: function(page) {
+		return function(req, res) {
+			res.render('page', {
+				currentPage: page
+			});
+		}
 	}
 }
