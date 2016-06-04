97f034af2ab5d055d6a4de675001a7b98cc6c969
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-06-04 16:26:07 +0300

    Adding simple routes test.

--- spec/routes-spec.js
+++ spec/routes-spec.js
@@ -1,10 +1,15 @@
 'use strict';
 
+const router = require('../app/routes');
+const request = require('supertest');
+const express = require('express');
+
+const app = express().use(router);
+
 describe('routes', function () {
   describe('GET /', function () {
     it('should render home page', function (done) {
-      expect(true).toBeTruthy('hello');
-      done();
+      request(app).get('/').expect(200, done);
     });
   });
 });
