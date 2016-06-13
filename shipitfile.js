07290fff7a09fb2c774d89898d48518cdb7c2b19
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-06-13 09:22:17 +0300

    Automating deployment.

--- shipitfile.js
+++ shipitfile.js
@@ -1,37 +1,58 @@
 'use strict';
 module.exports = function (shipit) {
-  require('shipit-deploy')(shipit);
   shipit.initConfig({
     default: {
-      workspace: '.',
-      repositoryUrl: 'https://github.com/evgeny-myasishchev/juliapagano',
-      ignores: ['.git', 'node_modules', 'log/*', 'public/assets/*', 'spec', 'tmp/*'],
-      keepReleases: 3,
+      workspace: './juliapagano',
+      servers: process.env.USER + '@hbox',
+      dockerRepository: 'evgenymyasishchev/juliapagano'
     },
     production: {
-      servers: 'juliapagano@hbox',
-      deployTo: '/apps/juliapagano',
+      container: 'juliapagano-prod',
+      port: 6000
     },
+    staging: {
+      container: 'juliapagano-stage',
+      port: 6010
+    }
   });
 
-  shipit.task('remote:restart', function () {
-    return shipit.remote('touch current/tmp/restart.txt');
+  // Perform new deployment
+  shipit.task('deploy:cold', function () {
+    const envFile = this.config.workspace + '/.env';
+    const port = this.config.port;
+    const repo = this.config.dockerRepository;
+    const container = this.config.container;
+    return shipit.remote('docker pull ' + this.config.dockerRepository)
+      .then(() => shipit.remote([
+        'docker run -d',
+        '--env-file', envFile,
+        '-p ' + port + ':3000',
+        '--name ', container, repo
+      ].join(' ')));
   });
 
-  shipit.task('remote:install-deps', function () {
-    return shipit.remote('source .shipit-profile && cd current && npm install');
+  // Perform update
+  shipit.task('deploy', function () {
+    const envFile = this.config.workspace + '/.env';
+    const port = this.config.port;
+    const repo = this.config.dockerRepository;
+    const container = this.config.container;
+    return shipit.remote('docker pull ' + this.config.dockerRepository)
+      .then(() => shipit.remote([
+        'docker stop', container
+      ].join(' ')))
+      .then(() => shipit.remote([
+        'docker rm', container
+      ].join(' ')))
+      .then(() => shipit.remote([
+        'docker run -d',
+        '--env-file', envFile,
+        '-p ' + port + ':3000',
+        '--name ', container, repo
+      ].join(' ')));
   });
 
-  shipit.task('remote:migrate', function () {
-    return shipit.remote('source .shipit-profile && cd current && bin/knex migrate:latest');
-  });
-
-  shipit.task('remote:seed', function () {
-    return shipit.remote('source .shipit-profile && cd current && bin/knex seed:run');
-  });
-
-  shipit.on('published', function () {
-    shipit.start('remote:install-deps');
-    shipit.remote('touch current/tmp/restart.txt');
+  shipit.task('deploy:restart', function () {
+    return shipit.remote('docker restart ' + this.config.container);
   });
 };
