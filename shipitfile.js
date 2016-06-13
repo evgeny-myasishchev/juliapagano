1099edfdc503a1381d18eb2270f05046ed2dbedc
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-06-14 01:30:26 +0300

    Making shipit file more flexible.

--- shipitfile.js
+++ shipitfile.js
@@ -1,4 +1,30 @@
 'use strict';
+
+function stopContainer(shipit) {
+  return shipit.remote('docker stop ' + shipit.config.container);
+}
+
+function startContainer(shipit) {
+  return shipit.remote('docker start ' + shipit.config.container);
+}
+
+function containerRestart(shipit) {
+  return stopContainer(shipit).then(() => startContainer(shipit));
+}
+
+function createContainer(shipit) {
+  const envFile = shipit.config.workspace + '/app.env';
+  const port = shipit.config.port;
+  const repo = shipit.config.dockerRepository;
+  const container = shipit.config.container;
+  return shipit.remote([
+    'docker run -d --restart=unless-stopped',
+    '--env-file', envFile,
+    '-p ' + port + ':3000',
+    '--name ', container, repo
+  ].join(' '));
+}
+
 module.exports = function (shipit) {
   shipit.initConfig({
     default: {
@@ -19,41 +45,26 @@ module.exports = function (shipit) {
 
   // Perform new deployment
   shipit.task('deploy:cold', function () {
-    const envFile = this.config.workspace + '/app.env';
-    const port = this.config.port;
-    const repo = this.config.dockerRepository;
-    const container = this.config.container;
-    return shipit.remote('docker pull ' + this.config.dockerRepository)
-      .then(() => shipit.remote([
-        'docker run -d',
-        '--env-file', envFile,
-        '-p ' + port + ':3000',
-        '--name ', container, repo
-      ].join(' ')));
+    return createContainer(shipit);
   });
 
   // Perform update
   shipit.task('deploy', function () {
-    const envFile = this.config.workspace + '/.env';
-    const port = this.config.port;
-    const repo = this.config.dockerRepository;
-    const container = this.config.container;
     return shipit.remote('docker pull ' + this.config.dockerRepository)
-      .then(() => shipit.remote([
-        'docker stop', container
-      ].join(' ')))
-      .then(() => shipit.remote([
-        'docker rm', container
-      ].join(' ')))
-      .then(() => shipit.remote([
-        'docker run -d',
-        '--env-file', envFile,
-        '-p ' + port + ':3000',
-        '--name ', container, repo
-      ].join(' ')));
+      .then(() => stopContainer(shipit))
+      .then(() => shipit.remote(['docker rm', shipit.config.container].join(' ')))
+      .then(() => createContainer(shipit));
   });
 
-  shipit.task('deploy:restart', function () {
-    return shipit.remote('docker restart ' + this.config.container);
+  shipit.task('deploy:create-container', function () {
+    return createContainer(shipit);
+  });
+
+  shipit.task('deploy:container-stop', () => stopContainer(shipit));
+
+  shipit.task('deploy:container-start', () => startContainer(shipit));
+
+  shipit.task('deploy:container-restart', function () {
+    return containerRestart(shipit);
   });
 };
