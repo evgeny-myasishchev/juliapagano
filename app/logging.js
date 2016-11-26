55a39c272c189e268fb67ad1b222283624fc6daa
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-11-27 01:28:57 +0200

    Adding helper code to filter assets logging.

--- app/logging.js
+++ app/logging.js
@@ -1,14 +1,35 @@
 'use strict';
 
-const config = require('config');
+const _ = require('lodash');
 const bunyan = require('bunyan');
-const loggingCfg = config.get('logging');
+const config = require('config');
 const path = require('path');
+
+const loggingCfg = config.get('logging');
 
 const rootCfg = { name: loggingCfg.name, streams: [] };
 
 if (loggingCfg.stdout.enabled) {
-  rootCfg.streams.push({ stream: process.stdout, level: loggingCfg.stdout.level || 'debug' });
+  //Uncomment below to filter out assets requests logging
+  //The best to do it is by extending bunyan middleware
+  // rootCfg.streams.push({
+  //   type: 'raw',
+  //   stream: {
+  //     write: function (data) {
+  //       const url = _.get(data, 'req.url');
+  //       if (url && url.startsWith('/assets')) return false;
+  //       if (_.get(data, 'res.statusCode') === 304) return false;
+  //
+  //       process.stdout.write(JSON.stringify(data));
+  //       process.stdout.write('\n');
+  //     }
+  //   },
+  //   level: loggingCfg.stdout.level || 'debug'
+  // });
+  rootCfg.streams.push({
+    stream: process.stdout,
+    level: loggingCfg.stdout.level || 'debug'
+  });
 }
 
 if (loggingCfg.file.enabled) {
