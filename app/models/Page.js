38bd60688153b77c0c666a6175a4f574030b4361
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-12-07 08:55:41 +0200

    Fixing lint warnings.

--- app/models/Page.js
+++ app/models/Page.js
@@ -1,27 +1,27 @@
 'use strict';
 
 class Page {
-  constructor() {
-
-  }
-
-  //Name that is shown in a nav menu as well as on the title bar
-  get name() {
-
-  }
-
-  //Title that is shown on the page
-  get title() {
-
-  }
-
-  static get(id) {
-
-  },
-
-  static create(data) {
-
-  }
+  // constructor() {
+  //
+  // }
+  //
+  // //Name that is shown in a nav menu as well as on the title bar
+  // get name() {
+  //
+  // }
+  //
+  // //Title that is shown on the page
+  // get title() {
+  //
+  // }
+  //
+  // static get(id) {
+  //
+  // },
+  //
+  // static create(data) {
+  //
+  // }
 }
 
 module.exports = Page;
