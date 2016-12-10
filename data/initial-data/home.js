e7e0be7e9290bdfa927321a3108e06f1bd9a5998
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-12-10 15:41:47 +0200

    Adding initial data. Fixing load script.

---
+++ data/initial-data/home.js
@@ -0,0 +1,26 @@
+/* eslint-disable max-len */
+
+module.exports = {
+  _id: 'home',
+  title: 'Home',
+  blocks: [
+    {
+      id: 'slogan',
+      type: 'content',
+      title: 'Slogan',
+      content: "Julia Pagano Photography <small id='slogan'>Real moments. Priceless memories.</small>",
+      flickr: {
+        photosetId: '72157669540784146',
+        size: 'Large',
+      },
+    },
+    {
+      id: 'about',
+      type: 'content',
+      title: 'About Me',
+      content:
+`<p>Hello! I am Julia.</p>
+<p>I am lifestyle portrait photographer specializing on children and family photography. I am based in  <a href='https://goo.gl/maps/8KZv6'>St Andrews, Fife</a>. Take a look <a href='/portfolio'>around</a> to see more of my work, and if it feels right - <a href="/contacts">do get in touch</a>. I'd be happy to capture your family, children and special moments for you to remember.</p>`,
+    },
+  ],
+};
---
+++ data/initial-data/portfolio.js
@@ -0,0 +1,15 @@
+/* eslint-disable max-len */
+
+module.exports = {
+  _id: 'portfolio',
+  title: 'Portfolio',
+  blocks: [
+    {
+      id: 'gallery',
+      flickr: {
+        photosetId: '72157667031701733',
+        size: 'Medium',
+      },
+    },
+  ],
+};
