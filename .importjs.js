4b6f4d088aeeb7730f3f687adc9b129c0afb89b9
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2017-03-21 15:14:32 +0200

    Adding import-js config

---
+++ .importjs.js
@@ -0,0 +1,9 @@
+module.exports = {
+  declarationKeyword : 'const',
+  importDevDependencies : true,
+  maxLineLength : 150,
+  aliases : {
+    Promise : 'bluebird',
+    request : 'superagent'
+  }
+}
